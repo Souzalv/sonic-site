5c138975d680686c540d9e95ab649b09cf54075b
Lucas Souza Alves <lucas.sa2005@gmail.com>
2024-03-12 11:41:06 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+let modal = document.getElementById("modal")
+let btn = document.getElementsByClassName("bnt")
+let fechado = document.getElementsBySpan("fechado")[0]
+
+
+
+
+function aberto() {
+    modal.style.display="block"
+}
+
+
+function fechar() {
+    modal.style.display="none"
+}
+
+
